526af6dbbaa98b900f2c8bebe1950b07e5d96e6c
Jakob Voss <voss@gbv.de>
2019-03-22 13:28:14 +0100

    Fix test

--- test/examples-ld.js
+++ test/examples-ld.js
@@ -6,8 +6,6 @@ const jsonld = require('jsonld')
 const types = "resource item concept scheme mapping concordance registry distribution occurrence bundle".split(" ")
 
 const context = JSON.parse(fs.readFileSync('./context.json'))
-const frame = JSON.parse(fs.readFileSync('./frame.json'))
-frame['@context'] = context
 
 // TODO: move to jskos-tools
 // or fix https://github.com/digitalbazaar/jsonld.js/issues/271
@@ -35,27 +33,9 @@ testExamples("JSON-LD examples", types, (objects, file, type) => {
     nquads = nquads.split("\n").filter( l => l ).sort().join("\n")+"\n"
     if (fs.existsSync(ntfile)) {
       let expect = fs.readFileSync(ntfile).toString()
-      assert.equal(nquads, expect)
+      assert.equal(expect, nquads)
     } else {
       console.log(nquads)
     }
-  })
-
-  objects.forEach( doc => {
-    jsonld.toRDF(doc, {format: 'application/n-quads'}, (err, nquads) => {
-      jsonld.fromRDF(nquads, {format: 'application/n-quads'}, (err, doc) => {
-        let options = {
-          expandContext: context,
-          omitDefault: true
-        }
-        jsonld.frame(doc, frame, options, (err, doc) => {
-          delete doc['@context']
-          //if ( (doc['@graph'] || []).length === 1 ) {
-          //  doc = doc['@graph'][0]
-          //}
-          console.log(doc)
-        })
-      })
-    })
   })
 })
